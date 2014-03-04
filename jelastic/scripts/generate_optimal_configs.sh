257c7da94bf8c2c60df4abe88c2e489d45fd8b88
Ihor Kolodyuk <ik@jelastic.com>
2014-03-04 18:37:34 +0200

    config optimizer fixed

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -3,11 +3,16 @@
 SED=$(which sed);
 
 #
-# config optimizer for redis
+# config optimizer for jetty8
 #
 
-#memory_total=`free -m | grep Mem | awk '{print $2}'`;
-#let maxmemory=memory_total-50; 
-#[ ! -z "$maxmemory" ] && export REDIS_MAXMEMORY=$maxmemory;
+JETTY_START_SCRIPT="${OPENSHIFT_JETTY8_DIR}.openshift/action_hooks/start";
+
+[ -z "$XMS" ] && { XMS=32; }
+memory_total=`free -m | grep Mem | awk '{print $2}'`;
+[ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}";  }
+
+$SED -i  "s/-Xms[0-9]*m/-Xms${XMS}m/g"  $JETTY_START_SCRIPT;
+$SED -i  "s/-Xmx[0-9]*m/-Xmx${XMX}m/g"  $JETTY_START_SCRIPT;
 
 
