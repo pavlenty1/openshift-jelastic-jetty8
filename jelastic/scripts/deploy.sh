3c211aab4e690669ad361b6e135ed48695fcaafc
Ihor Kolodyuk <ik@jelastic.com>
2014-03-06 15:25:32 +0200

    ff

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 # Simple deploy and undeploy scenarios for Jetty8
 
@@ -6,13 +6,14 @@ WGET=$(which wget);
 [ "x${context}" == "xROOT" ] && context="root";
 
 function _deploy(){
-        [ -e "${WEBROOT}/$context" ] && { 
-                rm -fr ${WEBROOT}/$context;
+        [ -f "${WEBROOT}/${context}.war" ] && {
+                rm -f "${WEBROOT}/${context}.war";
                 $WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
+                }
 }
 
 function _undeploy(){
-        [ -f ${WEBROOT}/${context}.war ] && rm -f ${WEBROOT}/${context}.war;
+        [ -f "${WEBROOT}/${context}.war" ] && rm -f "${WEBROOT}/${context}.war";
 }
 
 
